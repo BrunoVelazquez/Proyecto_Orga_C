9b4129c344b31d6cbed2e649bf530320430aa050
Bruno Velazquez <Brunovelazquez7@hotmail.com>
2019-09-10 12:47:19 -0300

    TDAlista

--- Estructuras/TDALista/lista.c
+++ Estructuras/TDALista/lista.c
@@ -1,7 +1,27 @@
-
+#include "lista.h"
+#include <stdlib.h>
 
 void crear_lista(tLista* l){
 
     l = (tLista) malloc(sizeof(struct celda));
+    (*l)->elemento = NULL;
+    (*l)->siguiente = NULL;
+    
 }
 
+void l_insertar(tLista l, tPosicion p, tElemento e){
+
+    
+
+}
+
+int l_longitud(tLista l){
+    int contador = 0;
+    tPosicion nodo = l->siguiente;
+    while (nodo != NULL)
+    {
+        nodo = nodo->siguiente;
+        contador++;
+    }
+    return contador;
+}
