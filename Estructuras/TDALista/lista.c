29e8c9e8eaa16bee890c56da9a5f3cfeda55f34f
Bruno Velazquez <Brunovelazquez7@hotmail.com>
2019-09-15 15:48:10 -0300

    Update lista.c

--- Estructuras/TDALista/lista.c
+++ Estructuras/TDALista/lista.c
@@ -2,17 +2,39 @@
 #include <stdlib.h>
 
 void crear_lista(tLista* l){
-
-    l = (tLista) malloc(sizeof(struct celda));
+    (*l) = (tLista) malloc(sizeof(struct celda));
+    // si *l es null hubo error!
+    if (*l == NULL)
+        exit(LST_ERROR_MEMORIA);
     (*l)->elemento = NULL;
     (*l)->siguiente = NULL;
-
 }
 
 void l_insertar(tLista l, tPosicion p, tElemento e){
+    tPosicion nodo_nuevo = (tPosicion) malloc(sizeof(struct celda));
+    if (nodo_nuevo == NULL)
+        exit(LST_ERROR_MEMORIA);
+    
+    tPosicion nodo_actual = l;
+    while ((nodo_actual->siguiente != NULL) && (nodo_actual->siguiente != p))
+        nodo_actual = nodo_actual->siguiente;
 
+    nodo_nuevo->elemento = e;
+    nodo_nuevo->siguiente = p; 
+    nodo_actual->siguiente = nodo_nuevo;
 
+}
 
+void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento)){
+    if (p == l_fin(l))
+        exit(LST_POSICION_INVALIDA);
+    
+    tPosicion nodo_actual = l;
+    while ((nodo_actual->siguiente != NULL) && (nodo_actual->siguiente != p))
+        nodo_actual = nodo_actual->siguiente;
+    
+    tElemento elemento = nodo_actual->elemento;
+    
 }
 
 int l_longitud(tLista l){
