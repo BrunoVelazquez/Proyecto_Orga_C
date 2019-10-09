ae09c5ad6032066279315a4d1fa762ede1f65f04
KatherinaVilca <katherinavilca@gmail.com>
2019-10-09 15:34:04 -0300

    Update ArbolTest.c

--- Estructuras/TDAArbol/ArbolTest.c
+++ Estructuras/TDAArbol/ArbolTest.c
@@ -7,6 +7,20 @@ void imprimir_Arbol (tArbol arbol){
     tNodo raiz= a_raiz(arbol);
     printf("Arbol: ");
 
+    while(raiz!= NULL)
+
 }
 
+void imprimirRec(tArbol a,tNodo nod, tLista lista){
+
+    tPosicion posLista= l_primera(lista);
+    while (posLista!=NULL){
+    tElemento elem= l_recuperar(lista,posLista);
+    tNodo nodoArbol=(tNodo) elem;
+    printf(nodoArbol->elemento,"  ", (nodoArbol->padre)->elemento);
+    }
+
+    imprimirRec(a,nodoArbol, nodoArbol->)
+ )
+}
 //tkm
