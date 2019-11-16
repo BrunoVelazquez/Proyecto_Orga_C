a6bc4aa7e56b40f079a1101296c83a6a32e5014e
KatherinaVilca <katherinavilca@gmail.com>
2019-11-16 19:23:11 -0300

    mejoras en partida

--- Estructuras/ProgramaPrincipal/main.c
+++ Estructuras/ProgramaPrincipal/main.c
@@ -24,93 +24,96 @@ void imprimir_tablero(tPartida p){
         printf("\n");
     }
 }
-int valor_utilidad(tPartida p, int jugador){
+
+void solicitar_movimiento(tPartida p){
+
+    int x;
+    int y;
+    int jugada_realizada;
+    int turno= p->turno_de;
+
+        if(turno==PART_JUGADOR_1){
+            printf("\n\n\n Turno de: %s",p->nombre_jugador_1);
+        }
+        else {
+                printf("\n\n\n Turno de: %s",p->nombre_jugador_2);
+             }
+
+    printf("\nIndique su jugada en fila: ");
+    scanf("%d",&x);
+    printf("Indique su jugada en columna: ");
+    scanf("%d",&y);
+
+    jugada_realizada=nuevo_movimiento(p,x,y);
+
+    while(jugada_realizada==PART_MOVIMIENTO_ERROR){
+
+        printf("\n Indique un valor de fila valido: ");
+        scanf("%d",&x);
+        printf("Indique un valor de columna valido: ");
+        scanf("%d",&y);
+        jugada_realizada=nuevo_movimiento(p,x,y);
+    }
+}
+
+int resultado_partida(tPartida p, int jugador){
+
 
     int ret=PART_EMPATE;
     int no_encontre_resultado=1;
 
      if(no_encontre_resultado==1){
-     if((  p->tablero->grilla[0][0])!=PART_SIN_MOVIMIENTO){
-
-          if (( p->tablero->grilla[0][0] ==  p->tablero->grilla[0][1] ==  p->tablero->grilla[0][2]) || ( p->tablero->grilla[0][0] == p->tablero->grilla[1][0] == p->tablero->grilla[2][0]) || (  p->tablero->grilla[0][0] == p->tablero->grilla[1][1] == p->tablero->grilla[2][2])){
+     if( ( p->tablero->grilla[0][0])!=PART_SIN_MOVIMIENTO){
+            //printf(" de casilla %d",p->tablero->grilla[0][0]);
+          if (( p->tablero->grilla[0][0]  ==  p->tablero->grilla[0][1] ==  p->tablero->grilla[0][2]) || ( p->tablero->grilla[0][0] == p->tablero->grilla[1][0] == p->tablero->grilla[2][0]) || (  p->tablero->grilla[0][0] == p->tablero->grilla[1][1] == p->tablero->grilla[2][2])){
+                     printf("adentro resultado part IF");
                 if((  p->tablero->grilla[0][0]) == jugador) {
-                        //ret= PAR;
+                        ret=1;
                         no_encontre_resultado=0;
                         printf("GANEE");
                 }
-/*
-                else {
-                        ret= IA_PIERDE_MAX;
-                        no_encontre_resultado=0;
-                     }
-                     */
+
             }
         }
      }//fin no encontre resultado.
 
-     if(no_encontre_resultado){
+     if(no_encontre_resultado==1){
 
      if(( p->tablero->grilla[1][1])!=PART_SIN_MOVIMIENTO){
 
             if ((( p->tablero->grilla[1][0]) == ( p->tablero->grilla[1][1]) == ( p->tablero->grilla[1][2])) || (( p->tablero->grilla[0][1]) == ( p->tablero->grilla[1][1]) == ( p->tablero->grilla[2][1])) || (( p->tablero->grilla[2][0]) ==( p->tablero->grilla[1][1]) == ( p->tablero->grilla[0][2]))) {
                 if(( p->tablero->grilla[1][0]) == jugador) {
-                        ret= IA_GANA_MAX;
+                        ret= 1;
                         no_encontre_resultado=0;
+                        printf("GANEE");
                 }
-        /*
-                else {
-                        ret= IA_PIERDE_MAX;
-                        no_encontre_resultado=0;
-                     }
-                     */
-            }
 
-        }
+            }
+     }
      }//fin no encontre resultado
 
 
-     if(no_encontre_resultado){
+     if(no_encontre_resultado==1){
 
      if(( p->tablero->grilla[2][2])!=PART_SIN_MOVIMIENTO){
 
         if ((( p->tablero->grilla[2][0]) == ( p->tablero->grilla[2][1]) == ( p->tablero->grilla[2][2])) || (( p->tablero->grilla[0][2]) == ( p->tablero->grilla[1][2]) == ( p->tablero->grilla[2][2]))){
                 if(( p->tablero->grilla[2][0])== jugador) {
-                        ret= IA_GANA_MAX;
+                        ret= 1;
                         no_encontre_resultado=0;
+                        printf("GANEE");
                 }
-                /*else {
-                        ret= part;
-                        no_encontre_resultado=0;
-                    }
-                    */
-
         }
      } //fin no encontre_resultado
+     }
 
-        //CASO DE IA NO TERMINO
-        if(no_encontre_resultado){
-            int i=0;
-            int j=0;
-            while(i<2 && no_encontre_resultado==0){
-                while(j<2 && no_encontre_resultado==0){
-                     if ( p->tablero->grilla[i][j]!= PART_SIN_MOVIMIENTO) j=j+1;
-                        else no_encontre_resultado=1; //quiere decir que IA NO TERMINO
-                }
-                i=i+1;
-            }
-
-            if(no_encontre_resultado==1) ret=PART_EN_JUEGO;
-    }
     return ret;
-}
+
 }
 
 int juego_modo_UvsU(tPartida p){
 
     int control_partida=PART_EN_JUEGO;
-    int jugada_realizada;
-    int x;
-    int y;
 
     printf("\n Tablero: \n");
     imprimir_tablero(p);
@@ -118,54 +121,39 @@ int juego_modo_UvsU(tPartida p){
     while(control_partida== PART_EN_JUEGO){
 
         if((p)->turno_de == PART_JUGADOR_1  ){
-        printf("\n Turno de: %s",p->nombre_jugador_1);
-         printf("\n Indique su jugada en fila: ");
-         scanf("%d",&x);
-         printf("Indique su jugada en columna: ");
-         scanf("%d",&y);
 
-         jugada_realizada=nuevo_movimiento(p,x,y);
+            solicitar_movimiento(p);
+            imprimir_tablero(p);
 
-         while(jugada_realizada==PART_MOVIMIENTO_ERROR){
+            int i= resultado_partida(p,p->turno_de);
 
-                printf("\n Indique un valor de fila valido: ");
-                scanf("%d",&x);
-                printf("\n Indique un valor de columna valido: ");
-                scanf("%d",&y);
-                jugada_realizada=nuevo_movimiento(p,x,y);
+            printf("resultado de metodo resultado %d",&i);
+             if(i==1){
+                    printf("adentro if");
+                control_partida= PART_GANA_JUGADOR_1;
+             }
+             else control_partida= PART_EMPATE;
+
+                    (p)->turno_de = PART_JUGADOR_2;
+
             }
-         imprimir_tablero(p);
-         control_partida=valor_utilidad(p,p->turno_de);
-         (p)->turno_de = PART_JUGADOR_2;
+        else{ // (p)->turno_de == PART_JUGADOR_2
+                solicitar_movimiento(p);
+                imprimir_tablero(p);
 
-        }
-    else{ // (p)->turno_de == PART_JUGADOR_2
+                if(resultado_partida(p,p->turno_de)==1){
+                        printf("adentro if");
+                control_partida= PART_GANA_JUGADOR_2;
 
-         printf("\n Turno de: %s",p->nombre_jugador_2);
-          printf("\n Indique su jugada en fila: ");
-         scanf("%d",&x);
-         printf("\n Indique su jugada en columna: ");
-         scanf("%d",&y);
+                }
 
-         jugada_realizada=nuevo_movimiento(p,x,y);
+                        (p)->turno_de = PART_JUGADOR_1;
 
-        while(jugada_realizada==PART_MOVIMIENTO_ERROR){
-                printf("\n Indique un valor de fila valido: ");
-                scanf("%d",&x);
-                printf("\n Indique un valor de columna valido: ");
-                scanf("%d",&y);
-
-             jugada_realizada=nuevo_movimiento(p,x,y);
-
-         }
-         imprimir_tablero(p);
-         control_partida=valor_utilidad(p,p->turno_de);
-         (p)->turno_de = PART_JUGADOR_1;
-        }
+            }
 
 
- }//fin while
-printf("termino  %i",p->estado);
+        }//fin while
+        printf("termino  %i",p->estado);
 return 0;
 }
 
